1551e10ef8027db39e9c0e21ee762cde12fb12f4
Jonathan Ghaly <60841107+JonathanGhaly@users.noreply.github.com>
2023-07-04 01:35:19 +0200

    update

--- search/functions_5.js
+++ search/functions_5.js
@@ -1,15 +1,15 @@
 var searchData=
 [
-  ['heat_5fequation_5f1d_5fp1_5fmpi_0',['heat_equation_1D_P1_MPI',['../physics_8h.html#a19b0b58d1e72c4b85fa1672b14555210',1,'physics.h']]],
-  ['heat_5fequation_5f1d_5fp1_5fopenmp_1',['heat_equation_1D_P1_OPENMP',['../physics_8h.html#a38c136c435bf6f862127e3cfaccc1a0e',1,'physics.h']]],
-  ['heat_5fequation_5f1d_5fserial_2',['heat_equation_1D_serial',['../physics_8h.html#a7390ae11a530bbf722997a1f42f310c2',1,'physics.h']]],
-  ['heat_5fequation_5f2d_5fp1_5fmpi_3',['heat_equation_2D_P1_MPI',['../physics_8h.html#a7e56e316652edb30dd742edf0156b912',1,'physics.h']]],
-  ['heat_5fequation_5f2d_5fp1_5fopenmp_4',['heat_equation_2D_P1_OPENMP',['../physics_8h.html#ada643ccfeeafe3a247c2a3fbf586aed5',1,'physics.h']]],
-  ['heat_5fequation_5f2d_5fserial_5',['heat_equation_2D_serial',['../physics_8h.html#a68cd88b12ebc4e2b124e5f5af1a713ad',1,'physics.h']]],
-  ['heat_5fequation_5fexecution_5ftime_5f1d_5fp1_5fmpi_6',['heat_equation_execution_time_1D_P1_MPI',['../physics_8h.html#a836fd752438eca48ff6893e21cec767f',1,'physics.h']]],
-  ['heat_5fequation_5fexecution_5ftime_5f1d_5fp1_5fopenmp_7',['heat_equation_execution_time_1D_P1_OPENMP',['../physics_8h.html#ab7206b80d87cfec3cb813f479fd0883a',1,'physics.h']]],
-  ['heat_5fequation_5fexecution_5ftime_5f1d_5fserial_8',['heat_equation_execution_time_1D_serial',['../physics_8h.html#aa095602956542bc2a024ff4da7f7eac5',1,'physics.h']]],
-  ['heat_5fequation_5fexecution_5ftime_5f2d_5fp1_5fmpi_9',['heat_equation_execution_time_2D_P1_MPI',['../physics_8h.html#ab5d9ea90faf46664d2c638d819137667',1,'physics.h']]],
-  ['heat_5fequation_5fexecution_5ftime_5f2d_5fp1_5fopenmp_10',['heat_equation_execution_time_2D_P1_OPENMP',['../physics_8h.html#aa7e26d1fc4e8ef65b81a0db162122b40',1,'physics.h']]],
-  ['heat_5fequation_5fexecution_5ftime_5f2d_5fserial_11',['heat_equation_execution_time_2D_serial',['../physics_8h.html#ab02315d90676197d2d723823d13cb4f9',1,'physics.h']]]
+  ['heat_5fequation_5f1d_5fp1_5fmpi_0',['heat_equation_1D_P1_MPI',['../physics_8h.html#a72581147c65395a93fe41e02e6751998',1,'physics.h']]],
+  ['heat_5fequation_5f1d_5fp1_5fopenmp_1',['heat_equation_1D_P1_OPENMP',['../physics_8h.html#a5f04da1fcf214753beb2f6376ebaf277',1,'physics.h']]],
+  ['heat_5fequation_5f1d_5fserial_2',['heat_equation_1D_serial',['../physics_8h.html#aa805fb790df29ef7dd4be6e5aa9a154b',1,'physics.h']]],
+  ['heat_5fequation_5f2d_5fp1_5fmpi_3',['heat_equation_2D_P1_MPI',['../physics_8h.html#a75ed163d3845df93b7780b0231f631cf',1,'physics.h']]],
+  ['heat_5fequation_5f2d_5fp1_5fopenmp_4',['heat_equation_2D_P1_OPENMP',['../physics_8h.html#aa518ce42953bc0c7e97e1b8a398d708c',1,'physics.h']]],
+  ['heat_5fequation_5f2d_5fserial_5',['heat_equation_2D_serial',['../physics_8h.html#ac0d56d3d53de808eb8b42609bf65e8af',1,'physics.h']]],
+  ['heat_5fequation_5fexecution_5ftime_5f1d_5fp1_5fmpi_6',['heat_equation_execution_time_1D_P1_MPI',['../physics_8h.html#a6c92f4828cb34dbd6fe9b8a2f02a65b7',1,'physics.h']]],
+  ['heat_5fequation_5fexecution_5ftime_5f1d_5fp1_5fopenmp_7',['heat_equation_execution_time_1D_P1_OPENMP',['../physics_8h.html#afb18b7a0a2c4c947357ffe8c443c7d59',1,'physics.h']]],
+  ['heat_5fequation_5fexecution_5ftime_5f1d_5fserial_8',['heat_equation_execution_time_1D_serial',['../physics_8h.html#a2bc0d919ddc674edf4adf1e615f03972',1,'physics.h']]],
+  ['heat_5fequation_5fexecution_5ftime_5f2d_5fp1_5fmpi_9',['heat_equation_execution_time_2D_P1_MPI',['../physics_8h.html#ae1f6ddbe4dda8fb7ed84b6fa83512c0b',1,'physics.h']]],
+  ['heat_5fequation_5fexecution_5ftime_5f2d_5fp1_5fopenmp_10',['heat_equation_execution_time_2D_P1_OPENMP',['../physics_8h.html#a92e42d46d00d387ad2d0af5fa440a004',1,'physics.h']]],
+  ['heat_5fequation_5fexecution_5ftime_5f2d_5fserial_11',['heat_equation_execution_time_2D_serial',['../physics_8h.html#a6008837e283ef1ecb77c66af659b5ddb',1,'physics.h']]]
 ];
